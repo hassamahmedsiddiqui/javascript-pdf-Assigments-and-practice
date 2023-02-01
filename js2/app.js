20b90e2baf65a76c0072d7cddc5cea7aaaf91e2f
hassamahmedsiddiqui <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-02-02 03:15:55 +0500

    Add files via upload

---
+++ js2/app.js
@@ -0,0 +1,86 @@
+// chapter 2 (variable for strings)
+          // Q1
+// var username = ""
+// console,console.log(username);
+         // Q2
+// var myname = "hassamAhmedSiddqiui"
+
+// console.log(myname)
+           // Q3
+// var message = "Hello World"
+// alert (message)
+// // console.log(message)
+          // Q4
+// var biodata = "John Doe";
+// alert(biodata)
+// biodata =" 15years Old";
+// alert(biodata) 
+// biodata ="Certfied web and appdevelopment";
+// alert(biodata)
+//           //  Q5
+// var pizz = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(pizz) 
+             // Q6
+// var email = 'My email is "Hassamahmed03@gmail.com"';
+// console.log(email)
+// alert(email)
+
+//             // Q7
+// var book = "I am trying to learn from the Book AsmarterWayToLearnJavaScript"
+// alert (book) 
+              // Q8
+// document.write("<h3>yes I can write JavaScript content through Html </h3>")
+// console.log(document.write)
+             // Q9
+// var sc = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(sc)
+
+
+
+            // Chapter no9: Variables For Number
+            // Q1
+// var age = "I'm 16 year old";
+// alert(age)
+//             // Q2
+// var visitors = "You have visited this time 15times"
+// alert(visitors)
+              // Q3
+// var birthyear ="My birth year is 2001 data type of my varibla number is declared "
+// document.write(birthyear)
+            //  Q4
+// var vname= "<h2>John Doe</h2>";
+// document.write(vname)
+// var ptitle = "orderd '<h5> 4 T-Shirts</h4>(s)'";
+// document.write(ptitle);
+// var cs= "On XYZ Clothing Store"
+// document.write(cs)
+
+
+             // Chapter No:4: Legal&Illegal 
+            // Q1
+
+// var name,age,address = ""
+// console.log(name,age,address)
+
+            // Q2
+            
+            // ---L E G A L---
+// var name=
+// var username=
+// var userresponse=
+// var user_respone=
+// var userre$ponse=
+
+          // ----ILLEGAL---
+// var N a me =
+// var 1number=
+// var var=
+// var 03456412160=
+// var bURGERS20=
+
+//          // Q3
+// document.write("<h1>Rules for naming JS Variables</h1>")
+// document.write("Variables name can only contain letters,numbers,$_For example:$my_1stVariable")
+// document.write("Variable names can only contain letter,$ or For Example:$name,_name,name")
+// document.write("variables mame are Case Sensitive")
+// document.write("variable names should notBE Js Variables")
