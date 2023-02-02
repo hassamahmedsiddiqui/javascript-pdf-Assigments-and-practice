26fb267474c60ff713a704307ee11130b3c70f31
hassamahmedsiddiqui <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-02-02 20:44:30 +0500

    java assigment 3

---
+++ js3/app.js
@@ -0,0 +1,41 @@
+//               // Chapter no 5
+//                   // Q1
+// var num = 3 + 5;
+// console.log(num)
+// document.write("Sum of 3 and 5 is 8")
+
+//                 // Q2
+// var num = 3 - 5;
+// console.log(num)
+
+// var num = 3 * 5;
+// console.log(num)
+
+// var num = 3 / 5;
+// console.log(num)
+
+// var num = 3 % 5;
+// console.log(num)
+
+
+                 // Q3
+// var num =              variable declaration
+// var num = 5
+// num++;
+// console.log(num)
+// var num = num++ + 7;
+// console.log(num)
+// var num = num - 1
+// console.log(num)
+// var num = num % 3 ;
+// console.log(num);
+// document.write("value after variable declaration is undefidend" + "<br>" + "value is 5" + "<br>" + "value after increment is 6" + "<br> " + "value after additon is 13" + "<br>" + "value after is decrement:12 " + "<br>")
+
+//                //    Q4
+// var ticketprice = 600 * 5 ;
+// console.log(ticketprice)
+// document.write("<h2>Total cost to buy 6 tickets to a movie is 3000pkr")
+
+
+               // Q5
+document.write("4x1=" + 4 * 1 + "")
