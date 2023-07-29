c3e8e63ff18834ab2497a9cd8d1e08630546118d
Hassam Ahmed <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-07-30 03:46:18 +0500

    Uploading Files using Fire Storage (classwork pratice)

--- app.js
+++ app.js
@@ -1,110 +1,68 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-// import { collection, addDoc, getFirestore, onSnapshot, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 
-// const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js";
 
-// };
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app)
-// const ids = [];
-// let total = 0;
+const firebaseConfig = {
 
-// const getTodos = () => {
-//     onSnapshot(collection(db, "todos"), (data) => {
-//         data.docChanges().forEach((todo) => {
-//             ids.push(todo.doc.id)
-//             if (todo.type === 'removed') {
-//                 let progress = document.getElementById("progress-width");
-//                 if (Math.round(100 / total) < 100) {
-//                     total--;
-//                     progress.style.width = Math.round(100 / total) + "%"
-//                     progress.innerText = Math.round(100 / total) + "%"
-//                 }
-//                 let dtodo = document.getElementById(todo.doc.id);
-//                 if (dtodo) {
-//                     dtodo.remove()
-//                 }
-//             } else if (todo.type === 'added') {
-//                 total++;
-//                 var list = document.getElementById("list");
-//                 list.innerHTML += `
-//                 <li id='${todo.doc.id}'>
-//                 <input class='todo-input' type='text' value='${todo.doc.data().value}' disabled/>
-//                 ${todo.doc.data().time}
-//                 <button onclick='delTodo("${todo.doc.id}")'>Delete</button> 
-//                 <button onclick='editTodo(this,"${todo.doc.id}")'>Edit</button>
-//                 </li>
-//                 `
-//             }
-//         })
-//     });
-// }
+};
 
-// getTodos()
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage();
 
 
-// const addTodo = async () => {
-//     try {
-//         var todo = document.getElementById("todo");
-//         var date = new Date()
-//         const docRef = await addDoc(collection(db, "todos"), {
-//             value: todo.value,
-//             time: date.toLocaleString()
-//         });
-//         todo.value = ""
-//     } catch (err) {
-//         console.log(err)
-//     }
-
-// }
-
-// async function delTodo(id) {
-//     await deleteDoc(doc(db, "todos", id));
-//     console.log("Todo deleted")
-// }
-// var edit = false;
-// async function editTodo(e, id) {
-
-//     if (edit) {
-//         await updateDoc(doc(db, "todos", id), {
-//             value: e.parentNode.childNodes[1].value
-//         });
-//         e.parentNode.childNodes[1].disabled = true;
-//         e.parentNode.childNodes[1].blur()
-//         e.parentNode.childNodes[5].innerHTML = "Edit"
-//         edit = false;
-//     } else {
-//         e.parentNode.childNodes[1].disabled = false;
-//         e.parentNode.childNodes[1].focus()
-//         e.parentNode.childNodes[5].innerHTML = "Update"
-//         edit = true;
-//     }
-// }
+let upload = document.getElementById("upload");
 
 
-// async function deleteAll() {
-//     let progress = document.getElementById("progress");
-//     progress.style.display = "block"
-//     var list = document.getElementById("list");
-//     list.innerHTML = ""
-//     let arr = []
-//     for (var i = 0; i < ids.length; i++) {
-//         arr.push(await deleteDoc(doc(db, "todos", ids[i])))
-//     }
-//     Promise.all(arr)
-//         .then((res) => {
-//             console.log("All data has been deleted")
-//         })
-//         .catch(err => {
-//             console.log("err")
-//         })
-// }
+const uploadFile = (file) => {
+    return new Promise((resolve, reject) => {
+        const mountainsRef = ref(storage, `images/saylani/${file.name}`);
+        const uploadTask = uploadBytesResumable(mountainsRef, file);
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                reject(error)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL);
+                });
+            }
+        );
+    })
+}
 
+upload.addEventListener('click', async () => {
+    try {
+        let file = document.getElementById("file");
+        const res = await uploadFile(file.files[0])
+        console.log("res-->", res)
+        let img = document.getElementById("img");
+        img.src = res;
+    } catch (err) {
+        console.log(err)
+    }
+})
 
+let deleteBtn = document.getElementById("delete-file")
+deleteBtn.addEventListener('click', () => {
+    const desertRef = ref(storage, 'images/Group 8.png');
 
-// window.addTodo = addTodo;
-// window.delTodo = delTodo;
-// window.editTodo = editTodo;
-// window.deleteAll = deleteAll;
-
+    deleteObject(desertRef).then((res) => {
+        console.log("res--->", res)
+    }).catch((error) => {
+        console.log(error)
+    });
+})
