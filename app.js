9d25afe35d131840121201f34265977f8768c34f
Hassam Ahmed <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-08-04 04:19:33 +0500

    Some Api fethcing practice

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, signOut }
+//   from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+//   import { doc, setDoc ,getFirestore} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"; 
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDuRKkLMTKOxdYJ9QIfDRHZsB36KbrsCqE",
+//   authDomain: "fir-assigment-e923c.firebaseapp.com",
+//   projectId: "fir-assigment-e923c",
+//   storageBucket: "fir-assigment-e923c.appspot.com",
+//   messagingSenderId: "97178963752",
+//   appId: "1:97178963752:web:83d5f2ec0d7312e139ca1d",
+//   measurementId: "G-TD856XT58T"
+// };
 
-const firebaseConfig = {
+// const app = initializeApp(firebaseConfig);
 
-};
-
-const app = initializeApp(firebaseConfig);
-
-const storage = getStorage();
-
-
-let upload = document.getElementById("upload");
-
-
-const uploadFile = (file) => {
-    return new Promise((resolve, reject) => {
-        const mountainsRef = ref(storage, `images/saylani/${file.name}`);
-        const uploadTask = uploadBytesResumable(mountainsRef, file);
-        uploadTask.on('state_changed',
-            (snapshot) => {
-                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                console.log('Upload is ' + progress + '% done');
-                switch (snapshot.state) {
-                    case 'paused':
-                        console.log('Upload is paused');
-                        break;
-                    case 'running':
-                        console.log('Upload is running');
-                        break;
-                }
-            },
-            (error) => {
-                reject(error)
-            },
-            () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                    resolve(downloadURL);
-                });
-            }
-        );
+// const auth = getAuth();
+// const db = getFirestore();
+let getNews = (search, page) => {
+    fetch(`https://api.newscatcherapi.com/v2/search?q=${search}&page_size=12&page=${page ? page : 1}`, {
+        headers: {
+            'x-api-key': 'tALHqDfvsKylZD8tlTKTuQokjTsT7MdX2hGRWPghptM'
+        }
     })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            let news = document.getElementById("news");
+            const articles = res.articles
+            for (var i = 0; i < articles.length; i++) {
+                const { media, title, summary } = articles[i];
+                console.log(articles[i])
+                news.innerHTML += `
+        <div class="card mt-2" style="width: 18rem;">
+        <img src="${media}" class="news-image card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">${title.slice(0, 20)}...</h5>
+        <p class="card-text">${summary.slice(0, 50)}...</p>
+        </div>
+        </div>
+        `
+            }
+        })
+        .catch(err => console.log(err))
 }
 
-upload.addEventListener('click', async () => {
-    try {
-        let file = document.getElementById("file");
-        const res = await uploadFile(file.files[0])
-        console.log("res-->", res)
-        let img = document.getElementById("img");
-        img.src = res;
-    } catch (err) {
-        console.log(err)
-    }
-})
+getNews()
 
-let deleteBtn = document.getElementById("delete-file")
-deleteBtn.addEventListener('click', () => {
-    const desertRef = ref(storage, 'images/Group 8.png');
+let page = 1;
 
-    deleteObject(desertRef).then((res) => {
-        console.log("res--->", res)
-    }).catch((error) => {
-        console.log(error)
-    });
-})
+let newsSearch = () => {
+    let news = document.getElementById("news");
+    let search = document.getElementById("search");
+    news.innerHTML = ""
+    getNews(search.value)
+}
+
+let loadMore = () => {
+    let search = document.getElementById("search");
+    page++
+    getNews(search.value, page)
+}
