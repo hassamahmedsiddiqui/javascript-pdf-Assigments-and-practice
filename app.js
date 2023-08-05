e2f6d3a821d260843509f27bd621983f59ed7769
Hassam Ahmed <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-08-05 17:22:36 +0500

    FireStorage practice

--- app.js
+++ app.js
@@ -1,64 +1,101 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth,createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { getFirestore,collection,addDoc ,getDocs, updateDoc,doc ,serverTimestamp  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, signOut }
-//   from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-//   import { doc, setDoc ,getFirestore} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"; 
+  const firebaseConfig = {
+    apiKey: "AIzaSyDuRKkLMTKOxdYJ9QIfDRHZsB36KbrsCqE",
+    authDomain: "fir-assigment-e923c.firebaseapp.com",
+    projectId: "fir-assigment-e923c",
+    storageBucket: "fir-assigment-e923c.appspot.com",
+    messagingSenderId: "97178963752",
+    appId: "1:97178963752:web:83d5f2ec0d7312e139ca1d",
+    measurementId: "G-TD856XT58T"
+  };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDuRKkLMTKOxdYJ9QIfDRHZsB36KbrsCqE",
-//   authDomain: "fir-assigment-e923c.firebaseapp.com",
-//   projectId: "fir-assigment-e923c",
-//   storageBucket: "fir-assigment-e923c.appspot.com",
-//   messagingSenderId: "97178963752",
-//   appId: "1:97178963752:web:83d5f2ec0d7312e139ca1d",
-//   measurementId: "G-TD856XT58T"
-// };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
 
-// const app = initializeApp(firebaseConfig);
 
-// const auth = getAuth();
-// const db = getFirestore();
-let getNews = (search, page) => {
-    fetch(`https://api.newscatcherapi.com/v2/search?q=${search}&page_size=12&page=${page ? page : 1}`, {
-        headers: {
-            'x-api-key': 'tALHqDfvsKylZD8tlTKTuQokjTsT7MdX2hGRWPghptM'
-        }
-    })
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            let news = document.getElementById("news");
-            const articles = res.articles
-            for (var i = 0; i < articles.length; i++) {
-                const { media, title, summary } = articles[i];
-                console.log(articles[i])
-                news.innerHTML += `
-        <div class="card mt-2" style="width: 18rem;">
-        <img src="${media}" class="news-image card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">${title.slice(0, 20)}...</h5>
-        <p class="card-text">${summary.slice(0, 50)}...</p>
-        </div>
-        </div>
-        `
-            }
-        })
-        .catch(err => console.log(err))
-}
 
-getNews()
+// let btnReg = document.getElementById('Register-btn')
+// btnReg.addEventListener('click',()=>{
+//     let name = document.getElementById('name')
+//     let phone = document.getElementById('phone')
+//     let email = document.getElementById('email')
+//     let password = document.getElementById('password')
+//     let userData = {
+//         name : name.value,
+//         phone: phone.value,
+//         email : email.value,
+//         password: password.value,
+        
+        
+//       }
+//       createUserWithEmailAndPassword(auth, userData.email, userData.password)
+//       .then(async(userCredential) => {
+//         // Signed in 
+//         const user = userCredential.user;
+//         console.log('user-->',user)
+//         try {
+//           const docRef = await addDoc(collection(db, "users"), {
+//             ...userData,
+//             uid : user.uid,
+//             timestamp: serverTimestamp(),
+        
+//       });
+//       console.log('time-->',time)
+//       console.log("Document written with ID: ", docRef.id);
+//     } catch (e) {
+//       console.error("Error adding document: ", e);
+//     }
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log('error-->',error)
+//     // ..
+//   });
+//     // console.log('user-->',user)
+// })
 
-let page = 1;
+// let getAllUsers =async ()=>{
 
-let newsSearch = () => {
-    let news = document.getElementById("news");
-    let search = document.getElementById("search");
-    news.innerHTML = ""
-    getNews(search.value)
-}
+//   const querySnapshot = await getDocs(collection(db, "users"));
+//   querySnapshot.forEach((doc) => {
+//     console.log(`${doc.id} =>`,doc.data());
+//   });
+// }
+// getAllUsers()
 
-let loadMore = () => {
-    let search = document.getElementById("search");
-    page++
-    getNews(search.value, page)
-}
+let btn = document.getElementById('update-btn')
+// console.log(btn)
+btn.addEventListener('click',async()=>{
+  console.log('current-->', auth.currentUser)
+  const id = auth.currentUser.uid
+  console.log('id-->',id)
+//   const washingtonRef = doc(db, "users", id);
+//   let updateName = document.getElementById('updateName')
+//   let updatePhone = document.getElementById('updatePhone')
+//   // console.log('ref-->',washingtonRef)
+//   try{  
+//   await updateDoc(washingtonRef, {
+//     name: updateName.value,
+//     phone: updatePhone.value,
+//   });
+//   console.log('updated')
+// }catch(err){
+//   console.log('error-->',err)
+// }
+})
+
+// import { doc, updateDoc, increment } from "firebase/firestore";
+
+// const washingtonRef = doc(db, "cities", "DC");
+
+// // Atomically increment the population of the city by 50.
+// await updateDoc(washingtonRef, {
+//     population: increment(50)
+// });
