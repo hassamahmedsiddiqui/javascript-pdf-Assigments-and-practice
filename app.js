ec4b80a5230b054cbd06b952f80686e600b6ea33
Hassam Ahmed <121582888+hassamahmedsiddiqui@users.noreply.github.com>
2023-09-28 06:24:32 -0700

    Js practice

---
+++ app.js
@@ -0,0 +1,140 @@
+// const nums = [1,2,3,4,5];
+// let sumNumber =  nums.map((num,i,arr)=>{
+//     console.log('arr-->',arr)
+//         return 'nums-->',num *5 , i;
+//     })
+// console.log(sumNumber)
+
+// const nums = [1,2,3,4,5];
+// let seq = nums.filter((num)=>{
+//     return num > 1
+// })
+// console.log(seq)
+//     num(5)
+// function num(b) {
+//     var a = 0
+//     console.log(b)
+
+//     console.log(a)
+// }
+// const b = 5
+// {
+//     const b = 10
+//     console.log(b)
+// }
+// test()
+// function test() {
+//     let a = 10;
+//     if(true){
+//         let a = 5
+//         console.log(a)
+//     }
+//     console.log(a)
+
+// }
+
+
+// let arr = [1,2,3,4,5,6];
+// let sum = arr.reduce((acc,curr,i,arr)=>{
+// return acc + curr;
+// },0)
+// console.log('sum-->',sum)
+
+//  hsm =(...arr) =>{
+//     console.log(arr)
+// }
+// hsm('hassam','ahmed','ali')
+
+// hsm  =>  console.log('hello')
+// hsm()
+
+// function sum(num){
+// return num * num;
+// }
+// function dSum (sq){
+//     console.log('square is ' + sq)
+// }
+
+// dSum(sum(8))
+
+// iife
+
+// {function hsm(sum){
+//     console.log(sum * sum)
+// }}(5);
+
+// function multiply (...nums){
+//     console.log(nums[0]+nums[1])
+// }
+// let arr = [1,2];
+// multiply(...arr)
+
+// let arr = [1,2,3,4,5,5,66,6,6,]
+// let newArr = new Set([arr])
+// console.log
+
+// let arr = [1, 2, 3, 4, 5, 5, 66, 6, 6];
+// let newArr = [...new Set(arr)];
+// console.log(newArr);
+
+
+// var arr = [1, 2, 2, 3, 4, 3, 7, 8, 2, 9, 6];
+// var uniqueArr = [];
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (uniqueArr.indexOf(arr[i]) === -1) {
+//         console.log(uniqueArr.indexOf(arr[i]) === -1)
+//         uniqueArr.push(arr[i]);
+//     }
+// }
+
+// console.log(uniqueArr);
+
+
+// var userInput = ; // Replace this with actual user input
+// var sum = 0;
+
+// for (var i = 0; i < userInput.length; i++) {
+//     sum += parseInt(userInput[i]);
+// }
+
+// console.log(sum);
+
+// var userInput = 1234;
+// let str = userInput.toString().split('');
+// console.log(userInput)
+
+// var userInput = 1238; // Assuming the user enters 1234 as a number
+
+// // Convert the input number to a string to extract individual digits
+// var inputString = userInput.toString();
+
+// // Initialize a variable to hold the sum
+// var sum = 0;
+
+// // Iterate through the string and add each digit to the sum
+// for (var i = 0; i < inputString.length; i++) {
+//   sum += parseInt(inputString[i]);
+// }
+
+// console.log(sum); // This will print the sum, which is 10 in this case
+
+
+// let user = 'hassam'
+// for (let i = 0; index < array.length; index++) {
+//     const element = array[index];
+
+// }
+
+// for(var i =0 ; i < user.length;i++){
+//     console.log(user[i])
+// }
+
+// let num = prompt('enter a number');
+// num.toString()
+// console.log(num)
+// let cal = 0
+// let str = num.toString();
+// for (var i = 0; i < str.length; i++) {
+//     console.log(cal += Number(str[i]))
+// }
